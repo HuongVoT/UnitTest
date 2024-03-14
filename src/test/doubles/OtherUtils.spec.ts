bab002fe415ab46189c8fcb2dd3207b383df28c0
Admin <admin@MacBook-Pro-cua-Admin-2.local>
2024-03-15 00:10:59 +0700

    Example about stub

---
+++ src/test/doubles/OtherUtils.spec.ts
@@ -0,0 +1,25 @@
+import { stringInfo } from '../../app/Utils';
+import { calculateComplexity } from '../../app/doubles/OtherUtils';
+
+describe('OtherUtile test suite', () => {
+  it('Calculate complexity', () => {
+    // testInfo is a stub: an incompleted input just used to test
+    const testInfo = {
+      length: 5,
+      extraInfo: {
+        f1: 'some infor',
+        f2: 2,
+      },
+    };
+    const actual = calculateComplexity(testInfo as any);
+    expect(actual).toBe(10);
+  });
+  it('Calculate complexity if input undefined', () => {
+    const testInfo = {
+      length: 4,
+      extraInfo: undefined,
+    };
+    const actual = calculateComplexity(testInfo as any);
+    expect(actual).toBe(0);
+  });
+});
